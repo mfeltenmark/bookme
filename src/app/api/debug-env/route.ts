f0c09c48a82e2440de1b8a4de60be1e95f299e34
mfeltenmark <mikael@techchange.io>
2026-03-09 17:47:47 +0100

    remove of debug

--- src/app/api/debug-env/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-export async function GET() {
-  return NextResponse.json({
-    hasCrmUrl: !!process.env.CRM_WEBHOOK_URL,
-    crmUrlPrefix: process.env.CRM_WEBHOOK_URL?.substring(0, 20),
-    hasCrmSecret: !!process.env.CRM_WEBHOOK_SECRET,
-  });
-}
